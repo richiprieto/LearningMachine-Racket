fd9192ecd69d0e7c78c3cb4d36283d98b6e2338e
richiprieto <richiprieto@crice.org>
2017-03-01 13:00:47 -0800

    Create ejemplosB2.scm

---
+++ ejemplosB2.scm
@@ -0,0 +1,27 @@
+(;principio de la lista de ejemplos.
+(;definición de los atributos
+(perspectiva (nublado soleado lluvioso));nominal
+(temperatura (15 30));nominal
+(humedad (30 85));nominal
+(viento (si no));nominal
+(clase (+ -));nominal
+);fin definición de atributos
+
+;;Ejemplos
+
+(soleado  25 79 si +)
+(soleado 25 75 si +)
+(lluvioso 20 21 no -)
+(lluvioso 35 20 no -)
+(soleado 20 79 si +)
+(soleado 16 75 si +)
+(soleado 28 40 si +)
+(lluvioso 25 13 no -)
+(nublado 17 31 si +)
+(soleado 23 31 si +)
+(lluvioso 12 90 no -)  
+(soleado 17 79 si +)
+(soleado 29 35 si +)
+(soleado 27 57 si +)
+(lluvioso 9 90 no -)
+(lluvioso 13 25 no -))
